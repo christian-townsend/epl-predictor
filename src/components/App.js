a5cad46d75a628407af2b9dd841198116a0b042c
christian-townsend <christiantownsend1995@gmail.com>
2021-07-11 21:53:32 +1000

    added player data

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,49 @@
 import logo from "../../src/logo.png";
 import "../styles/App.css";
 import React, { useState } from "react";
-import { Table } from "react-bootstrap";
+import { Table, Badge } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import scores from "./scores.json";
+import players from "./players.json";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <h1>
+        Example heading <Badge variant="primary">Players</Badge>
+      </h1>
       <Table striped bordered hover variant="dark">
         <thead>
           <tr>
-            {Object.keys(scores).map((item, index) => {
-              return <th>{item}</th>;
-            })}
+            <th>Name</th>
+            <th>Age</th>
+            <th>Position</th>
+            <th>Appearances</th>
+            <th>Goals</th>
+            <th>Assists</th>
+            <th>Penalty Goals</th>
+            <th>Penalty Misses</th>
+            <th>Goals involved in per 90</th>
+            <th>Assists involved in per 90</th>
+            <th>Goals per 90</th>
           </tr>
         </thead>
+
         <tbody>
-          {Object.keys(scores).map((item, index) => {
+          {players.map((player, index) => {
             return (
               <tr>
-                {Object.keys(scores).map((item, index) => {
-                  return <th>{item}</th>;
-                })}
+                <td>{player.full_name}</td>
+                <td>{player.age}</td>
+                <td>{player.position}</td>
+                <td>{player.appearances_overall}</td>
+                <td>{player.goals_overall}</td>
+                <td>{player.assists_overall}</td>
+                <td>{player.penalty_goals}</td>
+                <td>{player.penalty_misses}</td>
+                <td>{player.goals_involved_per_90_overall}</td>
+                <td>{player.assists_per_90_overall}</td>
+                <td>{player.goals_per_90_overall}</td>
               </tr>
             );
           })}
